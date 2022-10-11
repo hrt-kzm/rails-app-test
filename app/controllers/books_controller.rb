1bfaddea808991c46ee7031aafdcc496504bcc61
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-10-11 08:28:18 +0000

    [Middle]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,56 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new #モデルオブジェクトを作成し、インスタンス変数listに代入
-  end
-
-  def index
-    @books = Book.all #投稿されているbooksテーブルのデータを全て取得し、インスタンス変数booksに代入
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
-    @book = Book.find(params[:id]) #booksテーブルから指定したidのデータを取得し、インスタンス変数bookに代入する
+    @book = Book.find(params[:id])
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+  def index
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to 'books'
+    redirect_to '/books'
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:complete] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:complete] = 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
+
+  def update_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
